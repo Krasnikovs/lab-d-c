6ebd584e46013ca1bae4fe76ac8ffebfc7291b32
ubuntuchair <ralis3333@gmail.com>
2024-09-23 14:01:23 +0300

    prog

--- C1.cpp
+++ C1.cpp
@@ -8,8 +8,49 @@ Programma izveidota:
 #include <iostream>
 using namespace std;
 
+int mono(int A[8], int i, int sizeOfArr) {
+    
+    int count = 0;
+    int rev_count = 0;
+
+    for (i; i < sizeOfArr; i++) {
+        if (A[i] <= A[i + 1]) {
+            count += 1;
+        } else if (A[i] > A[i + 1]) {
+            int longest_count = count;
+            count = 0;
+        }
+
+        if (A[i] >= A[i + 1]) {
+            rev_count++;
+        } else if (A[i] < A[i + 1]) {
+            int longest_rev_count = rev_count;
+            rev_count = 0;
+        }
+
+        cout << count << rev_count << endl;
+    }
+
+    if (count >= rev_count) {
+        return count;
+    } else {
+        return rev_count;
+    }
+}
+
 int main() {
-        
+    int A[] = {4, 2, 8, 1, 3, 5, 15, 0};
+
+    int i;
+    cin >> i;
+
+    int sizeOfArr = sizeof(A)/sizeof(A[0]);
+
+    int count = mono(A, i - 1, sizeOfArr);
+
+    cout << count << endl;
+
+    return 0;
 }
 
 /*
